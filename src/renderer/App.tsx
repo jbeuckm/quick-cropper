7bf82f45012f589301f8411cde716aa9e90243ce
Joe Beuckman <jbeuckman@lm-jbeuckman.home>
2023-03-16 10:30:21 -0700

    show croppr

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Croppr from 'croppr';
+import './croppr.min.css';
 
 export default function App() {
   const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState();
+  const [croppr, setCroppr] = useState();
+  const cropprImg = useRef();
 
   useEffect(() => {
     window.electron.ipcRenderer.on('FILE_OPEN', (event, args) => {
@@ -10,11 +15,31 @@ export default function App() {
     });
   }, []);
 
+  useEffect(() => {
+    if (cropprImg.current) {
+      setCroppr(
+        new Croppr(cropprImg.current, {
+          startSize: [50, 50],
+          aspectRatio: 1,
+        })
+      );
+    }
+  }, [cropprImg.current]);
+
   return (
     <div>
       {images.map((image) => (
-        <img key={image.path} height="100" src={image.data} />
+        <img
+          key={image.path}
+          height="100"
+          src={image.data}
+          onClick={() => setSelectedImage(image)}
+        />
       ))}
+
+      {selectedImage && (
+        <img ref={cropprImg} width="100%" src={selectedImage.data} />
+      )}
     </div>
   );
 }
